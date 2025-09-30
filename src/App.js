d5c2ac16f7edaef132514f2e612214cc1d08b4b3
kcdk13579 <kcdk13579@gmail.com>
2025-09-30 13:25:06 +0530

    Added Dashboard by kcdk

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import LandingPage from "./components/LandingPage";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Profile from "./components/Profile";
+import Cart from "./components/Cart";
+import Account from "./components/Account";
+import Orders from "./components/Orders";
+import Help from "./components/Help";
+import Wishlist from "./components/Wishlist";
+import Search from "./components/Search";
+import Logout from "./components/Logout";
+import Coupons from "./components/Coupons";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/landing" element={<LandingPage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/coupons" element={<Coupons />} />
       </Routes>
     </Router>
   );
